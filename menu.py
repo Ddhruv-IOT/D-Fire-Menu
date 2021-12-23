6464b005f585696606cf3bf4c5e0dc1b889eaa07
Ddhruv-IOT <ddhruvarora2@gmail.com>
2021-12-24 02:46:55 +0800

    redesigning the system

--- menu.py
+++ menu.py
@@ -1,48 +1,26 @@
-import os
-import subprocess as sp
+import newmenu
+from newmenu import run_cmd
+import getpass
 
-os.chdir("/")
-os.chdir("/etc/yum.repos.d")
-print(os.system("pwd"))
+passwd = getpass.getpass("enter ur password :" )
 
-f = open("dvd.repo", "w")
-f.write(r"""
-[dvd1]
-baseurl=file:///run/media/root/RHEL-8-4-0-BaseOS-x86_64/AppStream
-gpgcheck=0
+if passwd != "ss":
+    print("password incorrect....")
+    exit()
 
-[dvd2]
-baseurl=file:///run/media/root/RHEL-8-4-0-BaseOS-x86_64/BaseOS
-gpgcheck=0
-
+print("""
+Press 1 to open Chrome
+Press 2 to open Atom
 """)
-f.close()
 
-os.system("yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm")
+x = input()
+if x == '1':
+    run_cmd("google-chrome --no-sandbox&")
+elif x == '2':
+    run_cmd("atom --no-sandbox&")
 
-
-os.chdir("/")
-os.chdir("/etc/yum.repos.d")
-print(os.system("pwd"))
-
-f = open("docker.repo", "w")
-f.write(r"""[docker]
-baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/
-gpgcheck=0
-""")
-f.close()
-
-os.system("cd")
-os.system("echo `yum -y install docker-ce --nobest --allowerasing`")
-
-
-os.system("yum -y install gcc perl make kernel-headers kernel-devel elfutils-libelf-devel")
-
-print(""" After this:
-1. Unmount iso using devices
-2. Mount Guest Addition Cd, click Run on PopUp
-3. Once its done force mount your ISO file again
-4. for increasing Screen Size click right ctrl + c and then maximize.
-5. Done!!!"""
-)
-
+#install_chrome()
+#configure_yum_repo()
+#config_epel()
+#config_docker()
+#install_depen()
