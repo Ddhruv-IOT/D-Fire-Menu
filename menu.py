f2941f8267f2c06af9143439491ca801a39bfa08
Ddhruv-IOT <ddhruvarora2@gmail.com>
2021-12-25 02:04:25 +0800

    updating the main menu

--- menu.py
+++ menu.py
@@ -2,24 +2,39 @@ import functions
 from functions import run_cmd
 import getpass
 
-passwd = getpass.getpass("enter ur password :" )
+def get_pass():
+    passwd = getpass.getpass("Enter your password: ")
+    return passwd
 
-if passwd != "ss":
-    print("password incorrect....")
-    exit()
+def veirfy_pass():
+    passwd = get_pass()
+    if passwd == 'q':
+        exit()
+    if passwd != "ss":
+        print("Incorrect Password")
+        print("Try again or Enter 'q' to exit")
+        veirfy_pass()
+    else:
+        main()
 
-print("""
-Press 1 to open Chrome
-Press 2 to open Atom
-""")
+def main():
+    while 1:
+        print("""
+        Press 1 to open Chrome
+        Press 2 to open Atom
+        """)
 
-x = input()
-if x == '1':
-    run_cmd("google-chrome --no-sandbox&")
-elif x == '2':
-    run_cmd("atom --no-sandbox&")
+        x = input()
+        if x == '1':
+            run_cmd("google-chrome --no-sandbox&")
+        elif x == '2':
+            run_cmd("atom --no-sandbox&")
+        elif x == '3':
+            run_cmd("date")
+        else:
+            break
 
-
+veirfy_pass()
 #install_chrome()
 #configure_yum_repo()
 #config_epel()
