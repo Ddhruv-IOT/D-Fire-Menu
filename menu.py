f8f817e038f2e27535f17ba7d3e929cd0f39cddd
Ddhruv-IOT <ddhruvarora2@gmail.com>
2021-12-30 01:25:42 +0800

    added more options and styles

--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
 import functions
 from functions import run_cmd
+from welcome import welcome
 import getpass
+import time
 
 def get_pass():
     passwd = getpass.getpass("Enter your password: ")
@@ -10,11 +12,31 @@ def veirfy_pass():
     passwd = get_pass()
     if passwd == 'q':
         exit()
+
+    print()
+    x = ["/", "-", "|", "\\"]
+    for i in range(5):
+        for i in x:
+            print("""Verifying User ....... """, end=" ")
+            print(f"{i}", end="")
+            time.sleep(0.1)
+            print(end="\r")
+    print("""Verifying User ....... """,end="\n")
+
     if passwd != "ss":
-        print("Incorrect Password")
-        print("Try again or Enter 'q' to exit")
+        print("""[❌] Invalid Password""")
+        time.sleep(0.4)
+        print("""[❌] Login Failed""")
+        time.sleep(0.4)
+        print("""[❌] Try again or Enter 'q' to exit""")
+        print()
         veirfy_pass()
     else:
+        print("""[✔] User Verified """)
+        time.sleep(0.4)
+        print("""[✔] Welcome Again """)
+        time.sleep(0.4)
+        print("""\n\t*********************** At Your Service *********************** """)
         main()
 
 def main():
@@ -22,36 +44,48 @@ def main():
         print("""
         Press 1 to open Chrome
         Press 2 to open Atom
+        Press 3 to run date command
+        Press 4 to run cal command
+        Press 5 to get set-up and config menu
+        Press q to exit
         """)
 
-        x = input()
-        if x == '1':
+        x = input("Enter Your choice: ")
+        if x == 'q':
+            exit()
+        elif x == '1':
             run_cmd("google-chrome --no-sandbox&")
         elif x == '2':
             run_cmd("atom --no-sandbox&")
         elif x == '3':
             run_cmd("date")
         elif x == '4':
+            run_cmd("cal")
+        elif x == '5':
             while 1:
                 print("""
-                1. for installing chrome
-                2. for configuring yum
-                3. for
-                4. for
-                * to go back to main menu """)
-                x = input("Enter ur choice")
+        Press 1 to setup chrome browser
+        Press 2 to config yum repo
+        Press 3 to setup EPEL repo
+        Press 4 to config docker
+        Press 5 to install dependencies for Guest Additions
+        Press * to go back to main menu\n""")
+
+                x = input("Enter your choice: ")
                 if x == '1':
                     functions.install_chrome()
                 elif x == '2':
                     functions.configure_yum_repo()
+                elif x == '3':
+                    functions.config_epel()
+                elif x == '4':
+                    functions.config_docker()
+                elif x == '5':
+                    functions.install_depen()
                 elif x == '*':
                     break
         else:
-            break
+            continue
 
+welcome()
 veirfy_pass()
-#install_chrome()
-#configure_yum_repo()
-#config_epel()
-#config_docker()
-#install_depen()
